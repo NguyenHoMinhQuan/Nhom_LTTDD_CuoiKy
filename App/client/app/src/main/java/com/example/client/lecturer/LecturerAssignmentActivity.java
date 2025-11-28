e05c7e982214c3c6ce2eea8e29e152485f8acf83
congkiet05 <congkiet05@gmail.com>
2025-11-28 16:40:34 +0700

    add LecturerAssignmentActivity

---
+++ App/client/app/src/main/java/com/example/client/lecturer/LecturerAssignmentActivity.java
@@ -0,0 +1,17 @@
+package com.example.client.lecturer;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.client.R;
+
+public class LecturerAssignmentActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.lecturer_assignment);
+    }
+}
