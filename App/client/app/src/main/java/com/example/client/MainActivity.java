01928bde9575f4fa3e2ada958dba4c527b196d05
Nguyễn Hồ Minh Quân <minhquan28062005@gmail.com>
2025-11-28 22:52:03 +0700

    Cap nhat vao trang Admin

--- App/client/app/src/main/java/com/example/client/MainActivity.java
+++ App/client/app/src/main/java/com/example/client/MainActivity.java
@@ -1,17 +1,7 @@
 package com.example.client;
 
-import android.os.Bundle;
-import android.widget.LinearLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private LinearLayout btnGoToUser, btnGoToCourse, btnGoToClass;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.admin_dashboard);
-    }
 }
