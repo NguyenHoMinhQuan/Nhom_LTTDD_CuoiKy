68fc8b825bf21b9e6433c11d7272ceeedd59ec9d
Nguyễn Hồ Minh Quân <minhquan28062005@gmail.com>
2025-12-25 00:47:25 +0700

    Hiển thị dữ liệu và CRUD cả ba giao diện quản lý

--- App/server/src/main/java/com/example/server/entity/ClassSchedule.java
+++ App/server/src/main/java/com/example/server/entity/ClassSchedule.java
@@ -1,12 +1,13 @@
 package com.example.server.entity;
 
+import java.time.LocalTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.time.LocalTime;
 
 @Entity
 @Table(name = "ClassSchedule")
